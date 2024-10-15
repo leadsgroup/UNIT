82c29f320adf44ceec8aab58a7f26034d180af15
avac3 <avac3@illinois.edu>
2024-10-15 14:26:11 -0500

    Updated App

--- src/app.py
+++ src/app.py
@@ -1,25 +1,91 @@
-# If you prefer to run the code online instead of on your computer click:
-# https://github.com/Coding-with-Adam/Dash-by-Plotly#execute-code-in-browser
 
-from dash import Dash, dcc, Output, Input  # pip install dash
-import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
+from dash import Dash, html, dcc
 
-# Build your components
-app = Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
-mytext = dcc.Markdown(children='')
-myinput = dbc.Input(value="# Hello World - let's build web apps in Python!")
+app = Dash(__name__)
 
-# Customize your own Layout
-app.layout = dbc.Container([mytext, myinput])
+app.layout = html.Div([
 
-# Callback allows components to interact
-@app.callback(
-    Output(mytext, component_property='children'),
-    Input(myinput, component_property='value')
-)
-def update_title(user_input):  # function arguments come from the component property of the Input
-    return user_input  # returned objects are assigned to the component property of the Output
+    # Map Types Section
+    html.Div([
+        html.H3('Map Types'),
+        dcc.Checklist(
+            options=[{'label': 'Topography', 'value': 'Topography'}],
+            value=['Topography'],
+            style={'margin-bottom': '10px'}
+        ),
+        html.Label('Race'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'Percent Hispanic/Latino', 'value': 'Hispanic/Latino'},
+                {'label': 'Percent Black/African American', 'value': 'Black/African American'},
+                {'label': 'Percent White', 'value': 'White'},
+            ],
+            value=['Hispanic/Latino', 'Black/African American', 'White'],
+            multi=True,
+            style={'margin-bottom': '20px'}
+        ),
+        html.Label('Income'),
+        dcc.Dropdown(
+            options=[
+                {'label': '$0 - $10,000', 'value': '0-10000'},
+                {'label': '$10,000 - $20,000', 'value': '10000-20000'},
+                {'label': '$20,000 - $30,000', 'value': '20000-30000'},
+                {'label': '$30,000 - $40,000', 'value': '30000-40000'},
+                {'label': '$40,000 - $50,000', 'value': '40000-50000'},
+                {'label': '$50,000 - $60,000', 'value': '50000-60000'},
+                {'label': '$60,000 - $70,000', 'value': '60000-70000'},
+                {'label': '$70,000 - $80,000', 'value': '70000-80000'},
+                {'label': '$80,000 - $90,000', 'value': '80000-90000'},
+                {'label': '$90,000 - $100,000', 'value': '90000-100000'},
+            ],
+            value=[],
+            multi=True,
+            style={'margin-bottom': '20px'}
+        ),
+        html.Label('Sensitive Areas'),
+        dcc.Dropdown(
+            options=[{'label': 'Churches', 'value': 'Churches'},
+                    {'label': 'Hospitals/Medical Centers', 'value': 'Hospitals/Medical Centers'},
+                    {'label': 'Schools/Universities', 'value': 'Schools/Universities'},],
+            value=[],
+            multi=True,
+            style={'margin-bottom': '20px'}
+        )
+    ], style={'padding': 10, 'flex': 1, 'border': '1px solid #d3d3d3'}),
 
- 
-if __name__=='__main__':
-    app.run_server()
+    # Noise Data Section
+    html.Div([
+        html.H3('Noise Data'),
+        dcc.Checklist(
+            options=[{'label': 'Vertiports', 'value': 'Vertiports'}],
+            value=['Vertiports'],
+            style={'margin-bottom': '10px'}
+        ),
+        dcc.Checklist(
+            options=[{'label': 'Noise Hemispheres', 'value': 'Noise Hemispheres'}],
+            value=[],
+            style={'margin-bottom': '10px'}
+        ),
+        html.Label('EVTOL Type'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'Stopped Rotor', 'value': 'Stopped Rotor'},
+                {'label': 'Tilt Rotor', 'value': 'Tilt Rotor'},
+                {'label': 'Hexacopter', 'value': 'Hexacopter'},
+            ],
+            value='Stopped Rotor',
+            style={'margin-bottom': '20px'}
+        ),
+        html.Label('Noise Level Per Tract'),
+        dcc.Slider(
+            min=0,
+            max=80,
+            step=1,
+            marks={i: f'{i} dB' for i in range(0, 81, 20)},
+            value=40
+        )
+    ], style={'padding': 10, 'flex': 1, 'border': '1px solid #d3d3d3'})
+
+], style={'display': 'flex', 'flexDirection': 'row'})
+if __name__ == '__main__':
+    app.run(debug=True)
